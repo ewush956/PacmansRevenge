f8158777ce7351f2da3da11316d8b652c1ea1a46
Evan Wushke <ewush956@mtroyal.ca>
2024-02-03 17:42:05 -0700

    clear not working, only has plot32 and letter

--- stage2/RASTER.C
+++ stage2/RASTER.C
@@ -1,16 +1,32 @@
 #include "raster.h"
+#include "TYPES.H"
 
-void plot_bitmap_32(ULONG32* base, int x, int y, const ULONG32* bitmap, unsigned int height) {
+void plot_bitmap_32(UINT32* base, int x, int y, const UINT32* bitmap, unsigned int height) {
     int row;
-    ULONG32* location = base + (y * 20) + (x >> 5);
+    UINT32* location = base + (y * 20) + (x >> 5);
 
-    if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT) {
+    if (x >= 0 && x < (SCREEN_WIDTH - SPRITE_WIDTH) && y >= 0 && y < (SCREEN_HEIGHT - SPRITE_HEIGHT) ) {
         for (row = 0; row < height; row++) {
         
             *location |= bitmap[row] >> (x % SPRITE_WIDTH);     
-            *(location + 1) |= bitmap[row] << SPRITE_WIDTH - (x % SPRITE_WIDTH);
-            location += 20;
-        
+            *(location + 1) |= bitmap[row] << (SPRITE_WIDTH - (x % SPRITE_WIDTH));
+            location += 20;        
         }
     }
+}
+void plot_letter(UINT8* base, int x, int y, const UINT8* bitmap) {
+    int row;
+    UINT8* location = base + (y * 80) + (x >> 3);
+
+    if (x >= 0 && x < (SCREEN_WIDTH - LETTER_WIDTH) && y >= 0 && y < (SCREEN_HEIGHT - LETTER_HEIGHT)) {
+        for (row = 0; row < LETTER_HEIGHT; row++) {
+        
+            *location |= bitmap[row] >> (x % LETTER_WIDTH);     
+            *(location + 1) |= bitmap[row] << LETTER_WIDTH - (x % LETTER_WIDTH);
+            location += 80;       
+        }
+    }
+}
+void plot_screen(UINT32* base, const UINT32* bitmap) {
+    return;
 }
