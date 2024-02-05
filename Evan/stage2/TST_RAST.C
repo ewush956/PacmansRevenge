2dd9533480db78b222cd0cf9b1d681832a96258b
Evan Wushke <ewush956@mtroyal.ca>
2024-02-05 10:02:07 -0700

    Cleaned up some testing code and added choppy animation :)

--- Evan/stage2/TST_RAST.C
+++ Evan/stage2/TST_RAST.C
@@ -17,33 +17,110 @@ const UINT32* sprites[] = {
 	tombstone, ghost_run, ghost_freeze
 };
 
+int arbitrary_numbers_400[] = {
+	141, 276, 392, 345, 72, 307, 51, 17, 198, 63, 
+	366, 304, 383, 71, 111, 190, 255, 39, 110, 307, 
+	297, 324, 393, 95, 386, 53, 222, 368, 85, 325, 
+	88, 100, 12, 92, 62, 275, 221, 131, 372, 300,
+	388, 262, 174, 356, 84, 121, 276, 142, 210, 
+	21, 336, 300, 392, 238, 92, 369, 88, 210, 
+	262, 174, 356, 84, 121, 276, 142, 400, 365,
+	18, 336, 300, 392, 238, 97, 369, 88, 210, 
+	8, 21, 57, 299, 238, 97, 369, 88, 210, 327,
+	333, 1, 32, 109, 238, 97, 369, 0, 210 
+};
+
+int arbitrary_numbers_640[] = {
+	339, 287, 372, 220, 597, 186, 93, 429, 258, 
+	70, 496, 20, 633, 612, 333, 61, 556, 404, 0,
+	369, 591, 383, 511, 433, 123, 320, 106, 268,
+	508, 122, 602, 280, 617, 482, 460, 218, 246, 
+	11, 138, 317, 213, 326, 36, 456, 94, 9, 253, 
+	269, 226, 381, 303, 29, 154, 251, 537, 604, 340, 
+	647, 466, 367, 144, 123, 626, 389, 168, 638, 
+	240, 389, 110, 281, 444, 565, 18, 211, 44, 
+	213, 625, 603, 98, 94, 47, 382, 119, 312, 9,
+	210, 240, 39, 255, 492, 515, 174, 377, 255, 37
+};
+
 void next_test(UINT32* base);
 void display_all_ascii(UINT8* base, int x0, int y0);
+void test_arbitrary_letter(UINT8* base);
 
 /*All below functions call next_test*/
 void speed_test(UINT32* base, UINT32 sprite[], unsigned int height);
 void display_all_sprites(UINT32* base);
 void test_boundaries(UINT32* base);
-void test_arbitrary(UINT32* base);
+void test_arbitrary_32(UINT32* base);
 int main()
 {
 	void *base32 = Physbase();
 	void *base8 = Physbase();
-	int x, y, i, index;
+	int x, y, i, j, index;
 
-	next_test(base32);
+	clear_screen_c(base32);
 
 	display_all_sprites(base32);
 	next_test(base32);
-	plot_bitmap_64(base32, 62, 80, test64, 64);
-	plot_bitmap_32(base32, 0, 80, tombstone, 32);
-	next_test(base32);
-	test_arbitrary(base32);
-	test_boundaries(base32);
 
-	/*Testing all letters*/
-	display_all_ascii(base8, 200, 150);
+	display_all_ascii(base8, 220, 150);
 	next_test(base32);
+
+	plot_bitmap_64(base32, 200+32, 80, test64, HITBOX_HEIGHT);
+	plot_bitmap_32(base32, 200, 80, tombstone, SPRITE_HEIGHT);
+	plot_bitmap_32(base32, 200, 80+32, tombstone, SPRITE_HEIGHT);
+	plot_bitmap_32(base32, 200+32, 80+64, tombstone, SPRITE_HEIGHT);
+	plot_bitmap_32(base32, 200+64, 80+64, tombstone, SPRITE_HEIGHT);
+	next_test(base32);
+
+	test_arbitrary_32(base32);
+	next_test(base32);
+
+	test_arbitrary_letter(base8);
+	next_test(base32);
+
+	test_boundaries(base32);
+	next_test(base32);
+
+	/*Code below animates our beloved angry boi <3*/
+
+	while(!Cconis()){
+
+		for (i = 0; i < 4; i++) {
+			plot_bitmap_32(base32, 200, 200, sprites[i], SPRITE_HEIGHT);
+			for (j = 0; j < 999; j++) {
+			}
+			clear_screen_c(base32);
+		}
+		for (i = 3; i >= 0; i--) {
+			plot_bitmap_32(base32, 200, 200, sprites[i], SPRITE_HEIGHT);
+			for (j = 0; j < 999; j++) {
+			}
+			clear_screen_c(base32);
+
+		}
+	}
+	Cnecin();
+	clear_screen_c(base32);
+	while (!Cconis()) {
+		for (i = 4; i < 8; i++) {
+			plot_bitmap_32(base32, 200, 200, sprites[i], SPRITE_HEIGHT);
+			for (j = 0; j < 899; j++) {
+			}
+			clear_screen_c(base32);
+		}
+
+		for (i = 7; i >= 4; i--) {
+			plot_bitmap_32(base32, 200, 200, sprites[i], SPRITE_HEIGHT);
+			for (j = 0; j < 899; j++) {
+			}
+			clear_screen_c(base32);
+
+		}
+	}
+	Cnecin();
+	clear_screen_c(base32);
+
 
 	/*testing speed and buffer limmits (commented because it's pretty slow)*/
 	/* speed_test(base32, ghost_4_down, SPRITE_HEIGHT); */
@@ -85,7 +162,6 @@ void speed_test(UINT32* base, UINT32 sprite[], unsigned int height) {
 		plot_bitmap_32(base, i, 300, sprite, height);
 		plot_bitmap_32(base, i, 350, sprite, height);
 	}
-	next_test(base);
 }
 void display_all_sprites(UINT32* base) {
     int x, y, i;
@@ -101,7 +177,6 @@ void display_all_sprites(UINT32* base) {
         y = startY + (i / spritesPerRow) * (spriteHeight + spacing);
         plot_bitmap_32(base, x, y, sprites[i], spriteHeight);
     }
-	next_test(base);
 }
 /*Tests allowable plotting limmits*/
 void test_boundaries(UINT32* base) {
@@ -114,44 +189,18 @@ void test_boundaries(UINT32* base) {
     plot_bitmap_32(base, (SCREEN_WIDTH - SPRITE_WIDTH) / 2, SCREEN_HEIGHT - SPRITE_HEIGHT, sprites[5], SPRITE_HEIGHT); 
     plot_bitmap_32(base, 0, (SCREEN_HEIGHT - SPRITE_HEIGHT) / 2, sprites[6], SPRITE_HEIGHT); 
     plot_bitmap_32(base, SCREEN_WIDTH - SPRITE_WIDTH, (SCREEN_HEIGHT - SPRITE_HEIGHT) / 2, sprites[7], SPRITE_HEIGHT); 
-
-    next_test(base);
 }
 /*Displays all prites within allowable range at aribitrary (x,y)*/
-void test_arbitrary(UINT32* base) {
-	plot_bitmap_32(base, 21, 20, sprites[0], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 92, 20, sprites[0], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 64, 101, sprites[1], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 36, 10, sprites[1], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 123, 181, sprites[2], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 87, 81, sprites[2], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 188, 268, sprites[3], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 247, 22, sprites[4], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 302, 100, sprites[5], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 368, 188, sprites[6], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 429, 264, sprites[7], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 481, 21, sprites[8], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 541, 102, sprites[9], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 27, 347, sprites[10], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 83, 322, sprites[11], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 148, 343, sprites[12], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 209, 328, sprites[13], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 260, 344, sprites[14], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 322, 227, sprites[15], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 52, 341, sprites[16], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 446, 10, sprites[17], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 507, 344, sprites[18], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 568, 90, sprites[19], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 159, 152, sprites[20], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 211, 213, sprites[21], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 272, 277, sprites[22], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 333, 333, sprites[23], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 399, 157, sprites[24], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 456, 212, sprites[25], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 513, 275, sprites[26], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 578, 334, sprites[27], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 607, 154, sprites[28], SPRITE_HEIGHT);
-	plot_bitmap_32(base, 601, 211, sprites[29], SPRITE_HEIGHT);
+void test_arbitrary_32(UINT32* base) {
+	int i;
 
-	next_test(base);
+	for (i=0; i < 95; i++) {
+		plot_bitmap_32(base, arbitrary_numbers_640[i], arbitrary_numbers_400[i], sprites[i % 30], SPRITE_HEIGHT);
+	}
+}
+void test_arbitrary_letter(UINT8* base) {
+	int i;
+	for(i = 0; i < 95; i++) {
+		plot_letter(base, arbitrary_numbers_640[i], arbitrary_numbers_400[i], font, i);
+	}
 }
