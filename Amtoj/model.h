8395a4ad60ef7be0b47a2880e2e89d4dba296941
Amtoj Punia <apuni866@mtroyal.ca>
2024-02-05 20:16:22 -0700

    data struct for the 'objects'

--- Amtoj/model.h
+++ Amtoj/model.h
@@ -1,1 +1,57 @@
+#ifndef MODEL_H
+#define MODEL_H
 
+#include "types.h"
+
+ 
+typedef struct 
+{
+	UINT16 x,y;					/*positon */
+	int delta_x,delta_y; 		/* displacement (horzontal or vertical) */
+	
+
+}Pacman;
+
+
+typedef struct 
+{
+	UINT16 x,y;			
+	int delta_x,delta_y; 		
+
+}Ghost;
+
+
+
+/*  (leave them for now prob wont need) 
+typedef struct 
+{
+	unsigned int x,y;			
+	int delta_x,delta_y; 		
+
+}CryingGhost;
+
+typedef struct 
+{
+	unsigned int x,y;			
+	int delta_x,delta_y; 		
+	
+
+}MoustacheGhost;;
+
+typedef struct 
+{
+	unsigned int x,y;			
+	int delta_x,delta_y; 		
+	
+
+}SmilingGhost;
+
+typedef struct 
+{
+	unsigned int x,y;		
+	int delta_x,delta_y; 		
+	
+}NeutralGhost;
+*/
+
+#endif
