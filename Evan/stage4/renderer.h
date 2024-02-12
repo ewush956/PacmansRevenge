b4ff0793d5b872798a22a9728334246af6cebfed
eWse <ewush956@mtroyal.ca>
2024-02-11 21:39:44 -0700

    Added Cell declarations, path booleans need to be updated.

--- Evan/stage4/renderer.h
+++ Evan/stage4/renderer.h
@@ -3,8 +3,9 @@
 #include "bitmaps.h"
 #include "types.h"
 
-#define X_OFFSET 16
-#define Y_OFFSET 8
+#define X_PIXEL_OFFSET 16
+#define Y_PIXEL_OFFSET 8
 
 void clock_tick_handle();
 void plot_map(UINT32* base, UINT32* map[][20]);
+
