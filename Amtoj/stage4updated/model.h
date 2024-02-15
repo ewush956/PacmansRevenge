4de4526ab933ab9845d64535d26f6f700c0dccd1
Amtoj Punia <apuni866@mtroyal.ca>
2024-02-15 00:40:23 -0700

    made some changes almost done render stuff only have ghost left

---
+++ Amtoj/stage4updated/model.h
@@ -0,0 +1,100 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include "types.h"
+#include "bitmaps.h"
+
+#define UP 1
+#define DOWN 2
+#define LEFT 3
+#define RIGHT 4
+
+#define MAP_TILE_LENGTH 40        /* 1 tile = 16 pixels*/ 
+#define MAP_TILE_HEIGHT 24   
+
+#define MAP_PIXEL_LENGTH 576	/* 18 horiz walls x 32 pixels = 576 */
+#define MAP_PIXEL_HEIGHT 352    /* 11 vert walls x 32 pixels = 352   */
+
+#define PIXELS_PER_CELL  16
+
+
+/* use the tile defn's for this */
+
+
+/*
+#define WALL 1;
+#define PATH 0;
+*/
+ /* move_ghost(int x, int y, Object *ref_to_object) */
+ 
+typedef struct {
+
+	int x_position, y_position;
+	bool open_path;
+	
+}Cell;
+
+typedef struct 
+{
+	UINT16 x, y;					/*positon */
+	int delta_x, delta_y; 		/* displacement (horzontal or vertical) */
+
+	int current_frame;		/* current sprite (state) index */
+	UINT8 direction;		/*UP, DOWN, LEFT, RIGHT*/
+	bool is_evil;
+	bool has_collided; 
+
+	int x_cell_index, y_cell_index; 
+	/*Check things like this:		
+	if (pacman->direction == DOWN) {check cell_map[pacman->x_cell_index][pacman->y_cell_index + 1].open_path}
+	*/
+
+}Pacman;
+
+typedef struct 
+{
+	UINT16 x,y;			
+	int delta_x,delta_y;
+
+	int current_frame;
+	UINT8 direction;
+	bool is_scared;		
+
+	int x_cell_index, y_cell_index;
+
+
+}Ghost;
+
+typedef struct{
+	UINT16 seconds, minutes,
+	MS_digit_minutes, LS_digit_minutes,
+	MS_digit_seconds, LS_digit_seconds; /*x positions*/
+
+}Timer;
+
+
+extern int tile_map[MAP_TILE_HEIGHT][MAP_TILE_LENGTH];
+extern Cell cell_map[MAP_TILE_HEIGHT][MAP_TILE_LENGTH];
+
+
+extern Pacman pacman;
+extern Ghost awkward_ghost;
+extern Ghost crying_ghost;
+extern Ghost moustache_ghost;
+extern Ghost cyclops_ghost;
+
+
+/* for object testing */
+void move_ghost_position (Ghost *ghost, Cell *cell_map[][MAP_TILE_LENGTH], int new_x, int new_y);
+void increase_ghost_velocity (Ghost *ghost, UINT16 vertical_velocity, UINT16 horizontal_velocity);
+void move_pacman_position (Pacman *pacman);/*,UINT16 delta_x, UINT16 delta_y); (may need this later so leaving parameters)*/ 
+
+/*UINT8 check_collision(Ghost *ghost, Pacman *pacman);*/
+UINT8 check_collision(Ghost *ghost, Pacman *pacman, UINT16 object_y_position, UINT16 object_x_position);
+/*bool check_collision(UINT16 object_x_position, UINT16 object_y_position);*/
+void init_map_cells(Cell cell_map[][MAP_TILE_LENGTH]);
+void set_ghost_path(Ghost *ghost, UINT16* path_array[][MAP_TILE_LENGTH], Cell cell_map[][MAP_TILE_LENGTH]);
+void init_ghost_paths(Ghost *ghost1, Ghost *ghost2, Ghost *ghost3, Ghost *ghost4, Cell cell_map[][MAP_TILE_LENGTH]);
+void update_cell(int* x_index, int* y_index);
+
+#endif
---
+++ Amtoj/stage4updated/renderer.c
@@ -0,0 +1,119 @@
+#include "renderer.h"
+#include "types.h"
+#include "model.h"
+#include "bitmaps.h"
+#include "RASTER.H"
+#include "map_plot.c"
+
+
+void render_map(UINT16* base) {
+    /*Called once at beggining of game*/
+  
+    int i, j, x, y;
+    x = X_PIXEL_OFFSET;
+    y = Y_PIXEL_OFFSET;
+    
+    for (i = 0; i < MAP_TILE_HEIGHT; i++) {
+        for(j = 0; j < MAP_TILE_LENGTH; j++) {
+            switch (tile_map[i][j]) {
+            case 1:
+                plot_bitmap_16(base, x, y, wall_single_16, WALL_SIZE);
+                break;
+            default:
+                break;
+            }
+            x += WALL_SIZE;
+        }
+        x = X_PIXEL_OFFSET;
+        y += WALL_SIZE;
+    }
+}
+void render_pacman(Pacman *pacman, Ghost *ghost) {
+    /*
+    Amtoj
+    1) Figure out pacman->delta_x and y based on direction 
+    2) call move_pacman_position and pass in new 
+    3) check return status -> if true, call handle_pacman_collision
+    
+    use check_next_cell(pacman->direction, pacman->x_cell_index, pacman->y_cell_index)*/
+
+
+    UINT8 collision_type; 
+
+    collision_type = check_collision(&ghost,&pacman,pacman->y,pacman->x);
+
+    if (collision_type == NO_COLLISION)
+        move_ghost_position(&pacman);
+    else
+         handle_pacman_collsion(collision_type);
+
+
+
+}
+void render_ghost(Ghost* ghost) {
+    /*
+    Amtoj
+    1) Figure out ghost->delta_x and y based on direction 
+    2) call move_ghost_position and pass in new
+    3) check return status -> if true, call handle_pacman_collision
+    
+    Use check_next_cell(ghost->direction, ghost->x_cell_index, ghost->y_cell_index)*/
+}
+void render_gameover() {
+    /*
+    Leave it for now 
+    */
+}
+void render_timer(Timer* timer) {
+    /*
+    Evan
+    every 70 clock ticks timer.second++*/
+    timer->seconds++;
+    if (timer->seconds == 60) {
+        timer->seconds = 0;
+        timer->minutes++;
+    }
+    if (timer->minutes < 10) {
+        /*plot_letter(base8, timer->MS_digit_minutes, 0, '0');
+        plot_letter(base8, timer->x, timer->y, timer->minutes);
+        */
+    }
+    else {
+        
+    }
+    /*plot_letter(base8, timer) */
+    
+}
+void clear_sprite(UINT32* base, int x, int y) {
+    /*
+    Evan
+    plot_bitmap_32(null_sprite_32)*/
+    plot_bitmap_32(base, x, y, null_sprite_32, SPRITE_HEIGHT);
+}
+bool check_next_cell(int dirrection, int x_cell_index, int y_cell_index){
+    /*returns true if the next cell in corresponding dirrection has an an open path, false otherwise*/
+    switch(dirrection) {
+        case UP:
+            if (cell_map[y_cell_index - 1][x_cell_index].open_path == FALSE) {
+                return FALSE;
+            }
+            break;
+        case DOWN:
+            if (cell_map[y_cell_index + 1][x_cell_index].open_path == FALSE) {
+                return FALSE;
+            }
+            break;
+        case LEFT:
+            if (cell_map[y_cell_index][x_cell_index - 1].open_path == FALSE) {
+                return FALSE;
+            }
+            break;
+        case RIGHT:
+            if (cell_map[y_cell_index][x_cell_index + 1].open_path == FALSE) {
+                return FALSE;
+            }
+            break;
+        return TRUE;
+    }
+}
+
