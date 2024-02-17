1272630243ed2267df94fc769888398d52a3e748
Amtoj Punia <apuni866@mtroyal.ca>
2024-02-16 22:59:52 -0700

    events .h .c with the documenttation

--- Evan/stage4/events.c
+++ Evan/stage4/events.c
@@ -49,13 +49,21 @@ void clock_tick_handle(UINT16* clock_count, Entites* entity) {
 
 
 
-/*
-*   We are using an XOR algorithm to generate a random number to mod it by 4 to select a random
-*   direction for the ghosts to choose from if they encounter multiple open paths after colliding with a wall 
+/*********************************************************************************
+*   This function handles collisions between a ghost and a wall in the game environment.
+*   If a collision with a wall occurs, the function determines possible directions the ghost
+*   can move to and selects one of them randomly. It uses an XOR algorithm to generate a random 
+*   number, which is then modded by the available directions to choose a random direction.
 *   
-*   NOTE this is not currently functional there is a bug with random generation in this and a logic error 
-*   
-**/
+*   Parameters:
+*      - collision_type: An integer representing the type of collision (e.g., WALL).
+*      - ghost: A pointer to the Ghost structure representing the ghost involved in the collision.
+*      - cell_map: A 2D array representing the game map.
+*      - xor_shift_struct: A pointer to the XOR shift structure used for random number generation.
+*
+*   Note: This function is currently not functional due to a bug with random number generation
+*         and a logic error.
+ *************************************************************************************/
 void handle_ghost_collision (UINT8 collision_type, Ghost* ghost, Cell cell_map[][MAP_TILE_LENGTH], Xor *xor_shift_struct) {
   
     UINT8 possible_direction = 0;
@@ -89,7 +97,7 @@ void handle_ghost_collision (UINT8 collision_type, Ghost* ghost, Cell cell_map[]
         }
 
 
-           random_number = random_number_generator(&xor_shift_struct);
+           random_number = random_number_generator(xor_shift_struct);
            random_number %= possible_direction; 
            possible_direction = random_number % possible_direction;
            ghost -> direction = possible_direction;
@@ -100,17 +108,17 @@ void handle_ghost_collision (UINT8 collision_type, Ghost* ghost, Cell cell_map[]
 
         printf("Not a wall collsion struct\n");
     }
-
 }
 
-/*
-*
-*
-*
-*
-*
-*
-**/
+/***************************************************************************
+*   This function handles collisions involving the Pacman character in the game environment.
+*   It updates Pacman's movement deltas based on the collision type and performs additional actions
+*   if Pacman collides with a ghost or an object.
+*   
+*   Parameters:
+*      - collision_type: An integer representing the type of collision (e.g., WALL, OBJECT).
+*      - pacman: A pointer to the Pacman structure representing the Pacman character.
+**********************************************************************************/
 void handle_pacman_collision(UINT8 collision_type, Pacman *pacman) {
 
     
@@ -136,13 +144,13 @@ void handle_pacman_collision(UINT8 collision_type, Pacman *pacman) {
     }
 
 }
-/* * * * * * * * * * * * *
+/***********************************************************
 *  Uses an XOR shift algorthm to generate a random number 
 *   -For randomizing direction of any ghost
 *    
 *   @return 'state' this is the random number that is returned
 *
-* * * * * * * * * * */
+***********************************************************/
 UINT32 random_number_generator(Xor *xor)
 {
 
--- Evan/stage4/events.h
+++ Evan/stage4/events.h
@@ -1,4 +1,4 @@
-
+/*
 #include "TYPES.H"
 #include "model.h"
 
@@ -12,3 +12,48 @@ typedef struct
 void handle_ghost_collision (UINT8 collision_type, Ghost* ghost, Cell cell_map[][MAP_TILE_LENGTH], Xor *xor_shift_struct);
 UINT32 random_number_generator(Xor *xor);
 void handle_pacman_collision(UINT8 collision_type, Pacman *pacman);
+*/
+
+/******************
+ * @file TYPES.h
+ * @brief Header file containing type definitions and function prototypes for the game model.
+ *******************/
+
+#ifndef TYPES_H
+#define TYPES_H
+#include "model.h"
+
+/*********************************************
+ * @struct Xor
+ * @brief Structure representing a random number generator using XOR algorithm.
+ * @var Xor::value
+ * Member 'value' holds the current state of the random number generator.
+ *******************************************/
+typedef struct {
+    UINT32 value; /**< Current state of the random number generator. */
+} Xor;
+
+/****************************************
+ * @brief Handles collisions involving a ghost character in the game environment.
+ * @param collision_type An integer representing the type of collision (e.g., WALL, OBJECT).
+ * @param ghost A pointer to the Ghost structure representing the ghost character.
+ * @param cell_map A 2D array of Cell structures representing the game map.
+ * @param xor_shift_struct A pointer to the Xor structure used for random number generation.
+ ***********************************/
+void handle_ghost_collision(UINT8 collision_type, Ghost *ghost, Cell cell_map[][MAP_TILE_LENGTH], Xor *xor_shift_struct);
+
+/**********************************
+ * @brief Generates a random number using the XOR algorithm.
+ * @param xor A pointer to the Xor structure used for random number generation.
+ * @return An unsigned 32-bit integer representing the generated random number.
+ *****************************/
+UINT32 random_number_generator(Xor *xor);
+
+/*************************************
+ * @brief Handles collisions involving the Pacman character in the game environment.
+ * @param collision_type An integer representing the type of collision (e.g., WALL, OBJECT).
+ * @param pacman A pointer to the Pacman structure representing the Pacman character.
+ **************************************/
+void handle_pacman_collision(UINT8 collision_type, Pacman *pacman);
+
+#endif /* TYPES_H */
