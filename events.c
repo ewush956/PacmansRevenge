f0727de4e85ef702dc0faef4335e80b998dbf2cb
ewush956 <ewush956@mtroyal.ca>
2024-02-21 15:16:55 -0700

    deleted lame ass comments

--- events.c
+++ events.c
@@ -4,51 +4,6 @@
 #include "TYPES.H"
 #include "events.h"
 #include <stdio.h>
-/*
-void clock_tick_handle(UINT16* clock_count, Entites* entity) {
-
-    Evan
-    Called 70 times per second, renders all sprites, determines states (next position/collision status, etc.)
-
-    do stuff to figure out delta x,y then render sprite
-
-    TODO
-    */
-    
-    /*
-    TODO FOR EVAN MAKE UPDATE_CELL UPDATE ALL THE CELSS AND TAKE IN ENTITY TYPE 
-    if (*clock_count % 4 == 0) {
-
-        Pacman* pac = entity->pacman;
-        Ghost* moustache = entity->moustache_ghost;
-        Ghost* crying = entity->crying_ghost;
-        Ghost* cyclops = entity->cyclops_ghost;
-        Ghost* awkward = entity->awkward_ghost;
-        
-        pac->direction = UP;
-        render_pacman(pac);
-        render_ghosts()
-        update_cell(&pac->x_cell_index, &pacman->y_cell_index);
-
-        update_cell(&crying_ghost->x_cell_index, &crying_ghost->y_cell_index);
-
-        update_cell(&moustache_ghost->x_cell_index, &moustache_ghost->y_cell_index);
-
-        update_cell(&cyclops_ghost->x_cell_index, &cyclops_ghost->y_cell_index);
-
-        update_cell(&awkward_ghost->x_cell_index, &awkward_ghost->y_cell_index);
-
-    }
-    if (clock_count == 70) {
-        render_timer();
-        clock_count = 0;
-    }
-    (*clock_count)++;
-
-}
-*/
-
-
 
 /*********************************************************************************
 *   This function handles collisions between a ghost and a wall in the game environment.
