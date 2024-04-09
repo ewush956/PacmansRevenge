e8445984847033b167bc2d70f9b1cf6e547027b7
ewush956 <ewush956@mtroyal.ca>
2024-04-08 17:27:47 -0700

    updated sound testing driver

--- music.h
+++ music.h
@@ -13,10 +13,8 @@
 extern const SoundCycle pacman_intro_treble[PACMAN_INTRO_TREBLE_LENGTH];
 extern const SoundCycle pacman_intro_bass[PACMAN_INTRO_BASS_LENGTH];
 
-
 int generate_frequency(int base_note, UCHAR8 target_octave);
 void play_intro();
-void play_jaws_theme();
 void stop_music();
 
 #endif
--- tst_snd.c
+++ tst_snd.c
@@ -2,10 +2,9 @@
 #include "TYPES.H"
 #include "music.h"
 #include "effects.h"
+#include "globals.h"
 #include <stdio.h>
 #include <osbind.h>
-
-typedef unsigned char GAME_STATE;
 
 #define QUIT ((UCHAR8)0)
 #define PLAYING ((UCHAR8)1)
@@ -13,31 +12,40 @@ typedef unsigned char GAME_STATE;
 ULONG32 get_time();
 GAME_STATE update_game_state(GAME_STATE new_state, char input);
 
-
 int main() {
     long old_ssp; 
     char input;
     GAME_STATE state = PLAYING;
     ULONG32 time_then = get_time(), time_now;
     ULONG32 time_elapsed;
-    int treble_song_length = sizeof(pacman_intro_treble) / sizeof(Note);
-    int bass_song_length = sizeof(pacman_intro_bass) / sizeof(Note);
+    int treble_song_length = PACMAN_INTRO_TREBLE_LENGTH;
+    int bass_song_length = PACMAN_INTRO_BASS_LENGTH;
     bool song_finished = FALSE;
 
-    int waka_repetitions = 10; 
+    int waka_repetitions = 5; 
     int current_index = 0;
     int time_left = 0;
 
-    MusicState trebleState = {0, 0};
-    MusicState bassState = {0, 0};
+    SoundState trebleState = {0, 0};
+    SoundState bassState = {0, 0};
     SoundState wakaState = {0, 0};
     SoundState wakaNoise = {0, 0};
-
+    printf("Press anykey to continue\n");
+    old_ssp = Super(0);
+    enable_channel(CHANNEL_C, TONE_ON, NOISE_OFF);
+    play_note(CHANNEL_C, C3_SHARP, 10);
+    play_note(CHANNEL_A, C4_SHARP, 12);
+    play_note(CHANNEL_B, A3, 15);
+    while (!Cconis()){
+    }
+    
+    Super(old_ssp);
 /* ~~~~~~~~~~~~~~~~~~~~~~~ PLAY PACMAN INTRO ~~~~~~~~~~~~~~~~~~~~~~ */
 
     old_ssp = Super(0);
     enable_channel(CHANNEL_B, TONE_ON, NOISE_OFF);
     enable_channel(CHANNEL_A, TONE_ON, NOISE_OFF);
+    enable_channel(CHANNEL_C, TONE_OFF, NOISE_OFF);
     Super(old_ssp);
 
     while (song_finished == FALSE) {
@@ -51,42 +59,14 @@ int main() {
             }
 
             old_ssp = Super(0);
-            update_music(CHANNEL_A, pacman_intro_treble, treble_song_length, &trebleState);
-            song_finished = update_music(CHANNEL_B, pacman_intro_bass, bass_song_length, &bassState); 
+            play_sound(CHANNEL_A, pacman_intro_treble, treble_song_length, &trebleState);
+            song_finished = play_sound(CHANNEL_B, pacman_intro_bass, bass_song_length, &bassState); 
             Super(old_ssp);
         }
     }
-    
-/* ~~~~~~~~~~~~~~~~~~~~~~~~ PLAY WAKA WAKA SOUND ~~~~~~~~~~~~~~~~~~~~~*/
-    old_ssp = Super(0);
-    enable_channel(CHANNEL_A, TONE_ON, NOISE_ON);
-    enable_channel(CHANNEL_B, TONE_ON, NOISE_ON);
-    Super(old_ssp);
+    /* ~~~~~~~~~~~~~~~~~~~~~~~ RANDOM CHORD ~~~~~~~~~~~~~~~~~~~~~~ */
 
-    while (waka_repetitions > 0) {
-        time_now = get_time();
-        time_elapsed = time_now - time_then;
 
-        if (time_elapsed >= 1) { 
-            time_then = time_now;
-
-            if (Cconis()) {
-                input = (char)Cnecin();
-            }
-
-            old_ssp = Super(0);
-            play_waka_sound(CHANNEL_A, waka_sound_cycle, WAKA_CYCLE_LENGTH, &wakaState); 
-            play_waka_sound(CHANNEL_B, waka_noise_cycle, WAKA_CYCLE_LENGTH, &wakaNoise); 
-            Super(old_ssp);
-
-            if (time_left <= 0) {
-                waka_repetitions--;
-
-                current_index = 0;
-                time_left = waka_sound_cycle[0].duration; 
-            }
-        }
-    }
 /* ~~~~~~~~~~~~~~~~~~~~~~~~ DONE TESTS ~~~~~~~~~~~~~~~~~~~~~~ */
     old_ssp = Super(0);
     set_master_volume(0);
@@ -107,7 +87,6 @@ ULONG32 get_time()
 	return timeNow;
 
 }
-/* Implementations for setNoteFrequency, setVolume, and enableChannel would go here */
 GAME_STATE update_game_state(GAME_STATE new_state, char input) {
 
     /*Do something that updates the gamestate*/
